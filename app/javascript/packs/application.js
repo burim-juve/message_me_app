a35e202e62b889bda32a8f081be7dc7c31ede06d
Burim <burim81@yahoo.com>
2022-01-27 22:39:22 +0100

    add navigation menu and dropdown functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,11 @@
 require("@rails/ujs").start()
 require("jquery").start()
 require("turbolinks").start()
-require("semantic-ui").start()
+require("semantic-ui-sass")
 require("@rails/activestorage").start()
 require("channels")
 
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
