1d3d24e4bfe3819eff96cbbb29ed2aa0b8fdbf77
Burim <burim81@yahoo.com>
2022-01-29 19:22:46 +0100

    Enable flash messages display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,9 @@ require("semantic-ui-sass")
 require("@rails/activestorage").start()
 require("channels")
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
+  })
